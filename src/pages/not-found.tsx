2b2583e8534b81312d24dd1670f9e6e1007bd8c4
patryk-kilian <patryk.kilian93@gmail.com>
2021-04-03 15:30:10 +0200

    styling not found page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,5 +1,28 @@
+import { Center, Box, Heading, Text, Button } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { DASHBOARD } from '../constants/routes';
+
 function NotFound() {
-  return <div>not found</div>;
+  return (
+    <Center h='100vh'>
+      <Box textAlign='center'>
+        <Heading as='h2' fontSize='120px' color='purple.800'>
+          OOPS!
+        </Heading>
+        <Text fontSize='5xl'>Page not found</Text>
+        <Button
+          as={RouterLink}
+          to={DASHBOARD}
+          variant='outline'
+          colorScheme='purple'
+          size='lg'
+          mt='4'
+        >
+          Visit Homepage
+        </Button>
+      </Box>
+    </Center>
+  );
 }
 
 export default NotFound;
